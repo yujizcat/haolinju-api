40d012a55f48f3c923555136f5ba11b0cb13124f
yuji zhou <yujizcat@outlook.com>
2022-05-03 12:27:24 +0800

    fix bugs on item model associate

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
-  #has_many :owner_bids, :class_name => "Bid", foreign_key => "owner_item_id"
-  #has_many :taker_bids, :class_name => "Bid", foreign_key => "taker_item_id"
+  # This allows to use `item.bids` to retrive all bids related to this item
+  has_many :bids, :class_name => "Bid", :foreign_key => "owner_item_id"
+  # has_many :taker_bids, :class_name => "Bid", :foreign_key => "taker_item_id"
 end
