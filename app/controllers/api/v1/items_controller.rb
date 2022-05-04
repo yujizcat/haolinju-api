c3d68b4740d12748dfc2ce9931a72fc049ef1dd5
yuji zhou <yujizcat@outlook.com>
2022-05-04 17:05:05 +0800

    item controller with testing add destroy update

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -8,21 +8,34 @@ class Api::V1::ItemsController < Api::V1::BaseController
 
   def create
     @item = Item.new(item_params)
-    @item.user = current_user
+    @item.user = @current_user
     if @item.save
-      redirect_to items_path(@item)
+      render json: @item
     else
       render :new
     end
   end
 
   def show
+    render json: @item
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    render json: @item
   end
 
   def destroy
-    @item = Item.find(params[:id])
-    @item.destroy
-    redirect_to items_path(@item)
+    begin
+      # p "item found"
+      @item = Item.find(params[:id])
+      @item.destroy
+      render json: @item
+      # request.referrer
+    rescue ActiveRecord::RecordNotFound => e
+      # p "item not found"
+    end
   end
 
   def myitems
@@ -38,6 +51,6 @@ class Api::V1::ItemsController < Api::V1::BaseController
   end
 
   def item_params
-    params.require(:item).permit(:name, :category, :description, :condition, :image_url, :value, :request, :freebie, :status)
+    params.require(:item).permit(:name, :category, :description, :condition, :image_url, :value, :request, :freebie, :status, :user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,16 @@ Rails.application.routes.draw do
     namespace :v1 do
       post "login", to: "sessions#login", as: :login
 
-      resources :items, only: [:index, :create, :show, :destroy]
+      resources :items, only: [:index, :create, :show, :update, :destroy]
       resources :bids, only: [:index, :create, :show, :destroy] do
         resources :reviews, only: [:index]
       end
-    
-      get    "users/:id/items",      to: "items#myitems"
-      patch  "bids/:id/decline",     to: "bids#decline"
-      patch  "bids/:id/accept",      to: "bids#accept"
-      patch  "items/:id/receive",    to: "items#receive"
+
+      get "users/:id/items", to: "items#myitems"
+      patch "bids/:id/decline", to: "bids#decline"
+      patch "bids/:id/accept", to: "bids#accept"
+      patch "items/:id/receive", to: "items#receive"
+      patch "items/:id", to: "items#update"
     end
   end
 end
