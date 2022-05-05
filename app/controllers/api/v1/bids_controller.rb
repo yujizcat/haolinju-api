4d4c4a46c977c7f02d4f6b2cf0cbb0a0b2d8ba82
yuji zhou <yujizcat@outlook.com>
2022-05-05 11:56:21 +0800

    bids controller

--- app/controllers/api/v1/bids_controller.rb
+++ app/controllers/api/v1/bids_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::BidsController < ApplicationController
+class Api::V1::BidsController < Api::V1::BaseController
   before_action :set_bid, only: [:show]
 
   def index
@@ -8,21 +8,25 @@ class Api::V1::BidsController < ApplicationController
 
   def create
     @bid = Bid.new(bid_params)
-    @bid.user = current_user
+    @bid.user = @current_user
     if @bid.save
-      redirect_to bids_path(@bid)
+      render json: @bid
     else
       render :new
     end
   end
 
   def show
+    render json: @bid
   end
 
   def destroy
-    @bid = Bid.find(params[:id])
-    @bid.destroy
-    redirect_to bids_path(@bid)
+    begin
+      @bid = Bid.find(params[:id])
+      @bid.destroy
+      render json: @bid
+    rescue ActiveRecord::RecordNotFound => e
+    end
   end
 
   def accept
@@ -35,5 +39,9 @@ class Api::V1::BidsController < ApplicationController
 
   def set_bid
     @bid = Bid.find(params[:id])
+  end
+
+  def bid_params
+    params.require(:bid).permit(:note, :status, :user_id, :owner_item_id, :taker_item_id)
   end
 end
