74cb8ad9333b8e8b3034f8fe71da0a49b70dc391
yuji zhou <yujizcat@outlook.com>
2022-05-10 16:33:45 +0800

    delete the test host img seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,9 @@ require "faker"
 require "securerandom"
 require "open-uri"
 
-file = URI.open("https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png")
-article = Article.new(title: "NES", body: "A great console")
-article.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+# file = URI.open("https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png")
+# article = Article.new(title: "NES", body: "A great console")
+# article.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
 # @article.photo.attached? #=> true/false
 # @article.photo.purge #=> Destroy the photo
 
